b50bfad2add2ca6c6d63f39f5c4218bd2a51f36f
samslow <shsvkdldj@likelion.org>
2020-07-04 18:11:30 +0900

    Update progress bar radius

--- src/components/ProgressBar.js
+++ src/components/ProgressBar.js
@@ -13,14 +13,14 @@ const Container = styled.div`
   width: 100%;
   height: 100%;
   background-color: #fff;
-  border-radius: 0;
+  border-radius: 10px;
   border: 1px #000 solid;
 `;
 const Pregress = styled.div`
   width: ${(props) => props.percent}%;
   height: 100%;
   background-color: #f4e0d0;
-  border-radius: 0;
+  border-radius: 10px;
   border-right: 1px #000 solid;
 `;
 
