5bc57b3e4a3a1d9ca8682ca7c9d751ebc868b393
samslow <shsvkdldj@likelion.org>
2020-07-06 00:33:06 +0900

    enving 5

--- create-env.js
+++
@@ -1,5 +0,0 @@
-const fs = require("fs");
-fs.writeFileSync(
-  "./.env",
-  `FIREBASE_API_KEY=${process.env.FIREBASE_API_KEY}\n`,
-);
--- src/firebase.js
+++ src/firebase.js
@@ -6,14 +6,14 @@ import "firebase/firestore";
 console.log("process.env", process.env);
 
 const config = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  databaseURL: process.env.FIREBASE_DB_URL,
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  storageBucket: process.env.FIREBASE_BUCKET,
-  messagingSenderId: process.env.FIREBASE_SENDER_ID,
-  appId: process.env.FIREBASE_APP_ID,
-  measurementId: process.env.FIREBASE_MEASURE_ID,
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DB_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_BUCKET,
+  messagingSenderId: process.env.REACT_APP_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASURE_ID,
 };
 
 firebase.initializeApp(config);
