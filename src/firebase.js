d462b442481cac815df79892b05b0976f857ed93
samslow <shsvkdldj@likelion.org>
2020-07-05 21:14:47 +0900

    Add firebase analytics

--- src/firebase.js
+++ src/firebase.js
@@ -1,4 +1,5 @@
 import firebase from "firebase/app";
+import "firebase/analytics";
 import "firebase/firestore";
 
 const config = {
@@ -13,6 +14,7 @@ const config = {
 };
 
 firebase.initializeApp(config);
+firebase.analytics();
 
 const firestore = firebase.firestore();
 
